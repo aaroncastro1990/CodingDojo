2e9a41ed3a0ce059065eacc6d7f1af9661ee6a19
David Madrigal <mc.david.madrigal@gmail.com>
2015-06-24 09:32:18 -0500

    add more test

--- ruby/potter /ruby/3/cart.rb
+++ ruby/potter /ruby/3/cart.rb
@@ -1,7 +1,6 @@
 class Cart
   attr_accessor :cart, :base_price
 
-
   def initialize(cart)
     @cart = cart || []
     @base_price = 8
@@ -9,6 +8,18 @@ class Cart
 
   def price
    return 0 if cart.empty?
-   cart.size * base_price
+   uniq_cart = cart.uniq
+   puts "uniq #{uniq_cart}"
+   puts "cart #{cart}"
+
+   if uniq_cart == cart
+
+     if uniq_cart.size.eql?(2)
+       base_price * uniq_cart.size * 0.95
+     end
+   else
+     cart.size * base_price
+   end
+
   end
 end
--- ruby/potter /ruby/3/test_cart.rb
+++ ruby/potter /ruby/3/test_cart.rb
@@ -13,4 +13,10 @@ class TestPrice < Test::Unit::TestCase
     assert_equal(8 * 2 , Cart.new([0,0]).price)
     assert_equal(8 * 3, Cart.new([1,1,1]).price)
   end
+
+  def test_simple_discounts
+    assert_equal(8 * 2 * 0.95, Cart.new([0, 1]).price)
+    
+  end
+
 end
